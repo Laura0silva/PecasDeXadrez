097de48a439d8ed98da150fe6e5076cf69be1b4f
Laura0silva <125465992+Laura0silva@users.noreply.github.com>
2025-03-19 20:15:28 +0000

    desafio movimento de peças de xadrez completo

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
+void torre(int casas){    //cria a função torre
+  if(casas > 0){          //condição para executar
+    printf("Torre para direita.\n");  //imprime movimento
+    torre(casas - 1);     //código para criar e sair do loop
+  }
+}
+
+void bispo(int casas){  //cria função bispo
+  if(casas > 0){        //condição para executar
+      for (int i = 1; i < 3; i++)  //loop externo q repete 2 vezes
+      {
+        printf("Bisco para cima,"); //imprime o movimento
+        while (i > 0) //loop interno, executado somente 1 vez
+        {
+          printf(" direita.\n");  //imprimo o movimento na mesma linha
+          break;      //para o loop interno
+        }
+      }
+    bispo(casas - 1); //código para criar e sair do loop (função)
+  }
+}
+
+void rainha(casas){ //cria a funçãi rainha
+  if(casas > 0){    //condição para executar
+    printf("Rainha para esquerda.\n"); //imprime movimento
+    rainha(casas - 1);  //código para criar e sair do loop
+  }
+}
+
 int main(){
-  int casas, i = 1;
+  int casas;
   //Apresentação do jogo
   printf("Bem vindo ao jogo de Xadrez!\n");
   printf("Neste jogo você pode escolher mover apenas 3 peças: Torre, Bispo e Rainha.\n");
@@ -13,42 +42,33 @@ int main(){
   //Pede ao usuário quantas casas deve se deslocar
   printf("A Torre irá se deslocar para direita, digite quantas casas: ");
   scanf("%d", &casas);
-  //Imprime o movimento de acordo com a solicitação do usuário
-  for(int i = 1; i <= casas; i++){
-    printf("Torre para direita.\n");
-  }
+  torre(casas);   // chama a função torre
+  
 
   //Pede ao usuário quantas casas deve se deslocar
   printf("Agora o Bispo irá se deslocar para cima e direita, digite quantas casas: ");
   scanf("%d", &casas);
-  //Imprime o movimento de acordo com a solicitação do usuário
-  while(i <= casas){
-    printf("Bisco para cima, direita.\n");
-    i++;
-  }
-
-  i = 1; //Reinicia o i para o próximo loop
+  if(casas % 2 != 0) printf("Bispo para cima, direita.\n"); //caso casas for impar, vai imprimi 1 vez para n haver erro
+  casas = casas / 2;    //Subtrai metade da quantidade para q haja necessidade de loop dentro da função
+  bispo(casas); //chama a função bispo
+  
   //Pede ao usuário quantas casas deve se deslocar
   printf("A Rainha irá se deslocar para esquerda, digite quantas casas: ");
   scanf("%d", &casas);
-  //Imprime o movimento de acordo com a solicitação do usuário
-  do
-  {
-    printf("Rainha para esquerda.\n");
-    i++;
-  } while (i <= casas);
+  rainha(casas);  //chama a função rainha
+  
 
   //Indica que o cavalo irá se mover
   printf("O Cavalo irá se deslocar para baixo e esquerda.\n");
 
   //Imprime o movimento do cavalo
-  for(int i = 1; i < 2; i++){
-    while (i < 3)
+  for(int i = 1; i < 2 && i > 0; i++){ //loop externo
+    while (i < 3)   //loop interno
     {
-      printf("Cavalo para baixo.\n");
+      printf("Cavalo para cima.\n");
       i++;
     }
-    printf("Cavalo para esquerda.\n");
+    printf("Cavalo para direita.\n");
   }
   
   return 0;
